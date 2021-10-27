9d24a4e57941baddb44a6911a283b713e4ece998
kweiwen <kweiwen.tseng@gmail.com>
2021-10-27 16:57:43 +0800

    refactor to resource folder

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <AudioParameterFloat.h>
+#include <CircularBuffer.h>
+//#include <DigitalDelayLine.h>
 
 int main()
 {
 	std::cout << "Hello World" << std::endl;
+
+	//AudioParameterFloat test("0x00", "note", 69);
+	//Oscillator osc();
+
 	return 0;
 }
