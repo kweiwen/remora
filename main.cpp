e6ff9d81aa50e5f5d8b706cbfe898870176fbd12
kweiwen <kweiwen.tseng@gmail.com>
2021-10-27 17:14:45 +0800

    test main

--- main.cpp
+++ main.cpp
@@ -11,5 +11,12 @@ int main()
 	AudioParameterFloat test("0x00", "note", 69);
 	Oscillator osc();
 
+	for (int i = 0; i < 128; i++)
+	{
+		test = i;
+		std::cout << test.cc_value << std::endl;
+		std::cout << test.value << std::endl;
+	}
+
 	return 0;
 }
