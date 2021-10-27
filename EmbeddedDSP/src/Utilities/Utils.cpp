8dccbc5c5044806f01d996defd077b83754c834b
kweiwen <kweiwen.tseng@gmail.com>
2021-10-27 17:07:15 +0800

    move source to header to fix linked issue

--- EmbeddedDSP/src/Utilities/Utils.cpp
+++ EmbeddedDSP/src/Utilities/Utils.cpp
@@ -1,71 +1,71 @@
-#include "Utils.h"
-
-bool Utils::isHexValid(const char* input_data)
-{
-	if(input_data[4] != '\0')
-		return false;
-	if(input_data[0] != '0')
-        return false;
-	if((input_data[1] != 'x') && (input_data[1] != 'X'))
-        return false;
-    if(!(input_data[2] >= '0' && input_data[2] <= '9') && !(input_data[2] >= 'A' && input_data[2] <= 'F') && !(input_data[2] >= 'a' && input_data[2] <= 'f'))
-        return false;
-    if(!(input_data[3] >= '0' && input_data[3] <= '9') && !(input_data[3] >= 'A' && input_data[3] <= 'F') && !(input_data[3] >= 'a' && input_data[3] <= 'f'))
-        return false;
-    return true;
-}
-
-int Utils::HexToInt(char input_data)
-{
-	if (input_data >= '0' && input_data <= '9')
-		return input_data - '0';
-    if (input_data >= 'A' && input_data <= 'F')
-        return input_data - 'A' + 10;
-    if (input_data >= 'a' && input_data <= 'f')
-    	return input_data - 'a' + 10;
-    return -1;
-}
-
-int Utils::HexsToInt(const char* input_data)
-{
-	if(isHexValid(input_data))
-	{
-		int MSB = HexToInt(input_data[2]);
-		int LSB = HexToInt(input_data[3]);
-		return (MSB * 16 + LSB);
-	}
-	else
-	{
-		return -1;
-	}
-}
-
-float Utils::getFractionalPart(float input_data)
-{
-	int INT = (int)input_data;
-	return input_data - (float)INT;
-}
-
-int Utils::sign(float input_data)
-{
-	if(input_data >= 0)
-	{
-		return 1;
-	}
-	else
-	{
-		return -1;
-	}
-}
-
-int Utils::sign(int input_data)
-{
-	if(input_data >= 0)
-	{
-		return 1;
-	}
-	else
-	{
-		return -1;
-	}
-}
+//#include "Utils.h"
+//
+//bool Utils::isHexValid(const char* input_data)
+//{
+//	if(input_data[4] != '\0')
+//		return false;
+//	if(input_data[0] != '0')
+//        return false;
+//	if((input_data[1] != 'x') && (input_data[1] != 'X'))
+//        return false;
+//    if(!(input_data[2] >= '0' && input_data[2] <= '9') && !(input_data[2] >= 'A' && input_data[2] <= 'F') && !(input_data[2] >= 'a' && input_data[2] <= 'f'))
+//        return false;
+//    if(!(input_data[3] >= '0' && input_data[3] <= '9') && !(input_data[3] >= 'A' && input_data[3] <= 'F') && !(input_data[3] >= 'a' && input_data[3] <= 'f'))
+//        return false;
+//    return true;
+//}
+//
+//int Utils::HexToInt(char input_data)
+//{
+//	if (input_data >= '0' && input_data <= '9')
+//		return input_data - '0';
+//    if (input_data >= 'A' && input_data <= 'F')
+//        return input_data - 'A' + 10;
+//    if (input_data >= 'a' && input_data <= 'f')
+//    	return input_data - 'a' + 10;
+//    return -1;
+//}
+//
+//int Utils::HexsToInt(const char* input_data)
+//{
+//	if(isHexValid(input_data))
+//	{
+//		int MSB = HexToInt(input_data[2]);
+//		int LSB = HexToInt(input_data[3]);
+//		return (MSB * 16 + LSB);
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+//
+//float Utils::getFractionalPart(float input_data)
+//{
+//	int INT = (int)input_data;
+//	return input_data - (float)INT;
+//}
+//
+//int Utils::sign(float input_data)
+//{
+//	if(input_data >= 0)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+//
+//int Utils::sign(int input_data)
+//{
+//	if(input_data >= 0)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <AudioParameterFloat.h>
 #include <CircularBuffer.h>
-//#include <DigitalDelayLine.h>
+#include <DigitalDelayLine.h>
+#include <Oscillator.h>
 
 int main()
 {
 	std::cout << "Hello World" << std::endl;
 
-	//AudioParameterFloat test("0x00", "note", 69);
-	//Oscillator osc();
+	AudioParameterFloat test("0x00", "note", 69);
+	Oscillator osc();
 
 	return 0;
 }
